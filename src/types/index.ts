7c4f6ef7ff43bca2dfa6b9048c7b08621c3ce103
turtle <qqaz50313@gmail.com>
2021-08-13 14:49:21 +0800

    feature: Add bootstrape & rss-parser to render basic UI

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface Podcast {
+  image: Record<"link" | "title" | "url", string>;
+  title: string;
+  description: string;
+  link: string;
+  copyright: string;
+  items: Episode[];
+}
+
+export interface Episode {
+  guid: string;
+  content: string;
+  ["content:encoded"]: string;
+  ["content:encodedSnippet"]: string;
+  contentSnippet: string;
+  creator: string;
+  ["dc:creator"]: string;
+  link: string;
+  pubDate: string;
+  title: string;
+  itunes?: {
+    author: string;
+    duration: string;
+    episode: string;
+    explicit: string;
+    image: string;
+    season: string;
+    summary: string;
+  };
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <template v-if="podcast">
+      <b-card
+        no-body
+        img-left
+        :imgSrc="podcast.image ? podcast.image.url : ''"
+        :img-width="250"
+        img-alt="Card image"
+      >
+        <b-card-body class="card-body">
+          <b-card-title titleTag="h2">{{ podcast.title }}</b-card-title>
+        </b-card-body>
+      </b-card>
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import Parser from "rss-parser";
+
+import { Podcast } from "../types";
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public url =
+    "https://feeds.soundon.fm/podcasts/954689a5-3096-43a4-a80b-7810b219cef3.xml";
+  public podcast: Podcast | null = null;
+
+  public async created() {
+    this.fetch();
+  }
+
+  public async fetch() {
+    let parser = new Parser<Podcast>();
+    let podcast = await parser.parseURL(this.url);
+    this.podcast = podcast;
+  }
+}
 </script>
